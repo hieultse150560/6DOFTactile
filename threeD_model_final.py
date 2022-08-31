73d22aeeb848c50d5bdca31b828c0a84a8db9c41
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-08-31 10:57:40 +0700

    Update threeD_model_final.py

--- threeD_model_final.py
+++ threeD_model_final.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
-# from utils_func import softmax
+from utils import softmax
 
 # Apply softmax for axis 0 of the volume
 def softmax(data):
