890955e119b2cd776c09a5a7350c88e62204d63d
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-08-31 10:45:11 +0700

    Update threeD_dataLoader.py

--- threeD_dataLoader.py
+++ threeD_dataLoader.py
@@ -32,6 +32,7 @@ def get_subsample(touch, subsample): # Tính trung bình theo từng cụm subsa
 
     return touch
 
+
 class sample_data_diffTask(Dataset):
     def __init__(self, path, window, subsample, mode):
         self.path = path
@@ -82,7 +83,42 @@ class sample_data_diffTask(Dataset):
 
         return tactile, heatmap, keypoint, tactile_frame # Lấy M frames xung quanh 1 middle frame + heatmap + keypoint của middle frame
 
+class sample_data_diffTask2(Dataset):
+    def __init__(self, path, window, subsample):
+        self.path = path
+        self.files = os.listdir(self.path)
+        self.subsample = subsample
+        touch = np.empty((1,96,96))
+        heatmap = np.empty((1,21,20,20,18))
+        keypoint = np.empty((1,21,3))
+        count = 0
 
+        for f in self.files:
+            count += 1
+            print (f, count)
+            data = pickle.load(open(self.path + f, "rb"))
+            touch = np.append(touch, data[0], axis=0)
+            heatmap = np.append(heatmap, data[1], axis=0)
+            keypoint = np.append(keypoint, data[2], axis=0)
+
+
+        self.data_in = [touch[1:,:,:], heatmap[1:,:,:,:,:], keypoint[1:,:,:]]
+        self.window = window
+
+    def __len__(self):
+        # return self.length
+        return self.data_in[0].shape[0]
+
+    def __getitem__(self, idx):
+        tactile = window_select(self.data_in[0],idx,self.window)
+        heatmap = self.data_in[1][idx,:,:,:,:]
+        keypoint = self.data_in[2][idx,:,:]
+        tactile_frame = self.data_in[0][idx,:,:]
+
+        if self.subsample > 1:
+            tactile = get_subsample(tactile, self.subsample)
+
+        return tactile, heatmap, keypoint, tactile_frame
 # dataset = sample_data_diffTask('/content/tactile_keypoint_data/tactile_keypoint_data', 10, 1)
 # i = 0
 # for i_batch, sample_batched in enumerate(dataset):
