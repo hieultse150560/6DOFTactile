4ab94f39b94a8e63a3b2d88ec0c30348b83f486a
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-09-07 21:26:58 +0700

    Update threeD_train_final.py

--- train/threeD_train_final.py
+++ train/threeD_train_final.py
@@ -145,14 +145,13 @@ if not args.eval:
     print ("Validation set size: ", len(val_dataset))
     
 #!!!!!!!!!!!!!!!!!!!!!
-# Chuẩn bị data for testing
-# if args.eval:
-#     test_path = args.test_dir
-#     mask = []
-#     test_dataset = sample_data_diffTask2(test_path, args.window, mask, args.subsample)
-#     # test_dataset = sample_data_diffTask(test_path, args.window, args.subsample) # use this line for the diffTask test set
-#     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
-#     print (len(test_dataset))
+Chuẩn bị data for testing
+if args.eval:
+    test_path = args.test_dir
+    mask = []
+    test_dataset = sample_data_diffTask(test_path, args.window, args.subsample, "test")
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8)
+    print ("Test set size: ", len(test_dataset))
 
 print (f"Name of experiment: {args.exp}, Window size: {args.window}, Subsample: {args.subsample}, Device: {device}")
 
@@ -184,127 +183,16 @@ if __name__ == '__main__':
     #     print("Now continue training")
 
     # Nếu chỉ đánh giá với tập test và pretrained model
-    if args.eval:
-        checkpoint = torch.load( args.exp_dir + 'ckpts/' + args.ckpt + '.path.tar')
-        model.load_state_dict(checkpoint['model_state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-        epochs = checkpoint['epoch']
-        loss = checkpoint['loss']
-        print (loss)
-        print("ckpt loaded:", args.ckpt)
-        print("Now running on val set")
-        model.eval()
-        avg_val_loss = []
-        avg_val_keypoint_l2_loss = []
-
-        tactile_GT = np.empty((1,96,96))
-        heatmap_GT = np.empty((1,21,20,20,18))
-        heatmap_pred = np.empty((1,21,20,20,18))
-        keypoint_GT = np.empty((1,21,3))
-        keypoint_pred = np.empty((1,21,3))
-        tactile_GT_v = np.empty((1,96,96))
-        heatmap_GT_v = np.empty((1,21,20,20,18))
-        heatmap_pred_v = np.empty((1,21,20,20,18))
-        keypoint_GT_v = np.empty((1,21,3))
-        keypoint_pred_v = np.empty((1,21,3))
-        keypoint_GT_log = np.empty((1,21,3))
-        keypoint_pred_log = np.empty((1,21,3))
-
-        bar = ProgressBar(max_value=len(test_dataloader)) # Thanh tiến trình
-
-        c = 0
-        for i_batch, sample_batched in bar(enumerate(test_dataloader, 0)):
-
-            tactile = torch.tensor(sample_batched[0], dtype=torch.float, device=device)
-            heatmap = torch.tensor(sample_batched[1], dtype=torch.float, device=device)
-            keypoint = torch.tensor(sample_batched[2], dtype=torch.float, device=device)
-            tactile_frame = torch.tensor(sample_batched[3], dtype=torch.float, device=device)
-
-
-            with torch.set_grad_enabled(False):
-                heatmap_out = model(tactile, device)
-                heatmap_out = heatmap_out.reshape(-1, 21, 20, 20, 18) # Output shape từ model
-                heatmap_transform = remove_small(heatmap_out.transpose(2,3), 1e-2, device)
-                keypoint_out, heatmap_out2 = softmax(heatmap_transform) 
-
-            loss_heatmap = torch.mean((heatmap_transform - heatmap)**2 * (heatmap + 0.5) * 2) * 1000 # Loss heatmap
-            heatmap_out = heatmap_transform
-
-            if i_batch % 100 == 0 and i_batch != 0:
-                print (i_batch, loss_heatmap)
-            # loss = loss_heatmap
-            # print (loss)
-
-            '''export image'''
-            # Nếu có in ra hình ảnh kết quả để kiểm nghiệm
-            if args.exp_image:
-                base = 0
-                imageData = [heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),
-                             heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),
-                             keypoint.cpu().data.numpy().reshape(-1,21,3),
-                             keypoint_out.cpu().data.numpy().reshape(-1,21,3),
-                             tactile_frame.cpu().data.numpy().reshape(-1,96,96)]
-
-                generateImage(imageData, args.exp_dir + 'predictions/image/', i_batch, base)
-
-            '''log data for L2 distance and video'''
-            # Lưu lại chồng các frame để in ra video
-            if args.exp_video:
-                if i_batch>50 and i_batch<60: #set range
-                    heatmap_GT_v = np.append(heatmap_GT, heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
-                    heatmap_pred_v = np.append(heatmap_pred, heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
-                    keypoint_GT_v = np.append(keypoint_GT, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
-                    keypoint_pred_v = np.append(keypoint_pred, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
-                    tactile_GT_v = np.append(tactile_GT,tactile_frame.cpu().data.numpy().reshape(-1,96,96),axis=0)
-
-            if args.exp_L2:
-                keypoint_GT_log = np.append(keypoint_GT_log, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
-                keypoint_pred_log = np.append(keypoint_pred_log, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
-
-            '''save data'''
-            # Nếu có lưu lại kết quả dự đoán để kiểm nghiệm (append)
-            if args.exp_data:
-                heatmap_GT = np.append(heatmap_GT, heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
-                heatmap_pred = np.append(heatmap_pred, heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
-                keypoint_GT = np.append(keypoint_GT, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
-                keypoint_pred = np.append(keypoint_pred, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
-                tactile_GT = np.append(tactile_GT,tactile_frame.cpu().data.numpy().reshape(-1,96,96),axis=0)
-
-                if i_batch % 20 == 0 and i_batch != 0: #set the limit to avoid overflow
-                    c += 1
-                    toSave = [heatmap_GT[1:,:,:,:,:], heatmap_pred[1:,:,:,:,:],
-                              keypoint_GT[1:,:,:], keypoint_pred[1:,:,:],
-                              tactile_GT[1:,:,:]]
-                    pickle.dump(toSave, open(args.exp_dir + 'predictions/data/' + args.ckpt + str(c) + '.p', "wb"))
-                    tactile_GT = np.empty((1,96,96))
-                    heatmap_GT = np.empty((1,21,20,20,18))
-                    heatmap_pred = np.empty((1,21,20,20,18))
-                    keypoint_GT = np.empty((1,21,3))
-                    keypoint_pred = np.empty((1,21,3))
-
-            avg_val_loss.append(loss.data.item())
-        print ("Loss:", np.mean(avg_val_loss))
-
-        '''output L2 distance'''
-        # Nếu có lưu lại kết quả distance giữa các keypoint để kiểm nghiệm (sau khi đã xếp chồng)
-        if args.exp_L2:
-            dis = get_keypoint_spatial_dis(keypoint_GT_log[1:,:,:], keypoint_pred_log[1:,:,:])
-            pickle.dump(dis, open(args.exp_dir + 'predictions/L2/'+ args.ckpt + '_dis.p', "wb"))
-            print ("keypoint_dis_saved:", dis.shape)
-
-        '''video viz'''
-        # Tạo video
-        if args.exp_video:
-            to_save = [heatmap_GT_v[1:,:,:,:,:], heatmap_pred_v[1:,:,:,:,:],
-                       keypoint_GT_v[1:,:,:], keypoint_pred_v[1:,:,:],
-                       tactile_GT_v[1:,:,:]]
-
-            print (to_save[0].shape, to_save[1].shape, to_save[2].shape, to_save[3].shape, to_save[4].shape)
-
-            generateVideo(to_save,
-                  args.exp_dir + 'predictions/video/' + args.ckpt,
-                  heatmap=True)
-
+#     if args.eval:
+#         checkpoint = torch.load( args.exp_dir + 'ckpts/' + args.ckpt + '.path.tar')
+#         model.load_state_dict(checkpoint['model_state_dict'])
+#         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+#         epochs = checkpoint['epoch']
+#         loss = checkpoint['loss']
+#         print (loss)
+#         print("ckpt loaded:", args.ckpt)
+#         print("Now running on val set")
+# Run with test set  
     # Quay lại nếu không phải là testing thì bắt đầu train
     train_loss_list = np.zeros((1))
     val_loss_list = np.zeros((1))
@@ -452,3 +340,115 @@ if __name__ == '__main__':
                                        '_' + str(args.lr) + '_' + str(args.window) + '.p', "wb" ))
 
         print("Train Loss: %.6f, Valid Loss: %.6f" % (avg_train_loss, avg_val_loss))
+        
+    model.eval()
+    avg_val_loss = []
+    avg_val_keypoint_l2_loss = []
+
+    tactile_GT = np.empty((1,96,96))
+    heatmap_GT = np.empty((1,21,20,20,18))
+    heatmap_pred = np.empty((1,21,20,20,18))
+    keypoint_GT = np.empty((1,21,3))
+    keypoint_pred = np.empty((1,21,3))
+    tactile_GT_v = np.empty((1,96,96))
+    heatmap_GT_v = np.empty((1,21,20,20,18))
+    heatmap_pred_v = np.empty((1,21,20,20,18))
+    keypoint_GT_v = np.empty((1,21,3))
+    keypoint_pred_v = np.empty((1,21,3))
+    keypoint_GT_log = np.empty((1,21,3))
+    keypoint_pred_log = np.empty((1,21,3))
+
+    bar = ProgressBar(max_value=len(test_dataloader)) # Thanh tiến trình
+
+    c = 0
+    for i_batch, sample_batched in bar(enumerate(test_dataloader, 0)):
+        tactile = torch.tensor(sample_batched[0], dtype=torch.float, device=device)
+        heatmap = torch.tensor(sample_batched[1], dtype=torch.float, device=device)
+        keypoint = torch.tensor(sample_batched[2], dtype=torch.float, device=device)
+        tactile_frame = torch.tensor(sample_batched[3], dtype=torch.float, device=device)
+
+
+        with torch.set_grad_enabled(False):
+            heatmap_out = model(tactile, device)
+            heatmap_out = heatmap_out.reshape(-1, 21, 20, 20, 18) # Output shape từ model
+            heatmap_transform = remove_small(heatmap_out.transpose(2,3), 1e-2, device)
+            keypoint_out, heatmap_out2 = softmax(heatmap_transform) 
+
+        loss_heatmap = torch.mean((heatmap_transform - heatmap)**2 * (heatmap + 0.5) * 2) * 1000 # Loss heatmap
+        heatmap_out = heatmap_transform
+
+        if i_batch % 100 == 0 and i_batch != 0:
+            print (i_batch, loss_heatmap)
+            # loss = loss_heatmap
+            # print (loss)
+
+        '''export image'''
+        # Nếu có in ra hình ảnh kết quả để kiểm nghiệm
+        if args.exp_image:
+            base = 0
+            imageData = [heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),
+                             heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),
+                             keypoint.cpu().data.numpy().reshape(-1,21,3),
+                             keypoint_out.cpu().data.numpy().reshape(-1,21,3),
+                             tactile_frame.cpu().data.numpy().reshape(-1,96,96)]
+
+            generateImage(imageData, args.exp_dir + 'predictions/image/', i_batch, base)
+
+        '''log data for L2 distance and video'''
+        # Lưu lại chồng các frame để in ra video
+        if args.exp_video:
+            if i_batch>50 and i_batch<60: #set range
+                heatmap_GT_v = np.append(heatmap_GT, heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
+                heatmap_pred_v = np.append(heatmap_pred, heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
+                keypoint_GT_v = np.append(keypoint_GT, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
+                keypoint_pred_v = np.append(keypoint_pred, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
+                tactile_GT_v = np.append(tactile_GT,tactile_frame.cpu().data.numpy().reshape(-1,96,96),axis=0)
+
+        if args.exp_L2:
+            keypoint_GT_log = np.append(keypoint_GT_log, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
+            keypoint_pred_log = np.append(keypoint_pred_log, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
+
+        '''save data'''
+        # Nếu có lưu lại kết quả dự đoán để kiểm nghiệm (append)
+        if args.exp_data:
+            heatmap_GT = np.append(heatmap_GT, heatmap.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
+            heatmap_pred = np.append(heatmap_pred, heatmap_out.cpu().data.numpy().reshape(-1,21,20,20,18),axis=0)
+            keypoint_GT = np.append(keypoint_GT, keypoint.cpu().data.numpy().reshape(-1,21,3),axis=0)
+            keypoint_pred = np.append(keypoint_pred, keypoint_out.cpu().data.numpy().reshape(-1,21,3),axis=0)
+            tactile_GT = np.append(tactile_GT,tactile_frame.cpu().data.numpy().reshape(-1,96,96),axis=0)
+
+            if i_batch % 20 == 0 and i_batch != 0: #set the limit to avoid overflow
+                c += 1
+                toSave = [heatmap_GT[1:,:,:,:,:], heatmap_pred[1:,:,:,:,:],
+                              keypoint_GT[1:,:,:], keypoint_pred[1:,:,:],
+                              tactile_GT[1:,:,:]]
+                pickle.dump(toSave, open(args.exp_dir + 'predictions/data/' + args.ckpt + str(c) + '.p', "wb"))
+                tactile_GT = np.empty((1,96,96))
+                heatmap_GT = np.empty((1,21,20,20,18))
+                heatmap_pred = np.empty((1,21,20,20,18))
+                keypoint_GT = np.empty((1,21,3))
+                keypoint_pred = np.empty((1,21,3))
+
+        avg_val_loss.append(loss.data.item())
+    print ("Loss:", np.mean(avg_val_loss))
+
+    '''output L2 distance'''
+    # Nếu có lưu lại kết quả distance giữa các keypoint để kiểm nghiệm (sau khi đã xếp chồng)
+    if args.exp_L2:
+       dis = get_keypoint_spatial_dis(keypoint_GT_log[1:,:,:], keypoint_pred_log[1:,:,:])
+       pickle.dump(dis, open(args.exp_dir + 'predictions/L2/'+ args.ckpt + '_dis.p', "wb"))
+       print ("keypoint_dis_saved:", dis.shape)
+
+     '''video viz'''
+    # Tạo video
+    if args.exp_video:
+       to_save = [heatmap_GT_v[1:,:,:,:,:], heatmap_pred_v[1:,:,:,:,:],
+                      keypoint_GT_v[1:,:,:], keypoint_pred_v[1:,:,:],
+                      tactile_GT_v[1:,:,:]]
+
+       print (to_save[0].shape, to_save[1].shape, to_save[2].shape, to_save[3].shape, to_save[4].shape)
+
+       generateVideo(to_save,
+                  args.exp_dir + 'predictions/video/' + args.ckpt,
+                  heatmap=True)
+
