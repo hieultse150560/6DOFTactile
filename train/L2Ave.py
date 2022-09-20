774ef908473e9b630d32c8ee71106ffc8250d002
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-09-20 20:49:49 +0700

    Update L2Ave.py

--- train/L2Ave.py
+++ train/L2Ave.py
@@ -3,5 +3,6 @@ import numpy as np
 with open('./predictions/L2/singlePerson_0.0001_10_best_dis.p', 'rb') as f:
   dis = pickle.load(f)
   print(type(dis), dis.shape)
-  print(np.mean(dis, axis = 0))
-  
+  full = np.mean(np.abs(dis), axis = 0)
+  print(full)
+  print(np.mean(full, axis = 0))
