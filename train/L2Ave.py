015451758475c33c35f954c6fb6244b3fa796b6d
Hieu Trung Le <62134254+hieultse150560@users.noreply.github.com>
2022-09-20 20:53:03 +0700

    Update L2Ave.py

--- train/L2Ave.py
+++ train/L2Ave.py
@@ -6,3 +6,7 @@ with open('./predictions/L2/singlePerson_0.0001_10_best_dis.p', 'rb') as f:
   full = np.mean(np.abs(dis), axis = 0)
   print(full)
   print(np.mean(full, axis = 0))
+  full = np.mean(dis, axis = 0)
+  print(full)
+  print(np.mean(full, axis = 0))
+
