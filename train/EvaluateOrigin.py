8e5e891849964e2819274e5a1dbc301406cf4dc6
Hieu Le <62134254+hieultse150560@users.noreply.github.com>
2022-12-22 15:11:58 +0700

    Update EvaluateOrigin.py

--- train/EvaluateOrigin.py
+++ train/EvaluateOrigin.py
@@ -23,7 +23,7 @@ warnings.filterwarnings("ignore")
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--exp_dir', type=str, default='./', help='Experiment path') #Change
-parser.add_argument('--exp', type=str, default='singlePeopleOrigin_22_12', help='Name of experiment')
+parser.add_argument('--exp', type=str, default='singlePeopleOrigin', help='Name of experiment')
 parser.add_argument('--lr', type=float, default=1e-4, help='Learning rate') 
 parser.add_argument('--batch_size', type=int, default=32, help='Batch size,256')
 parser.add_argument('--weightdecay', type=float, default=1e-3, help='weight decay')
